b67fcca7b63ee8c95243ec287502e622fd499168
KRISHCHHIPA-CODER <chhipakrish96@gmail.com>
2024-07-06 08:14:49 +0530

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [bmi, setBmi] = useState(0);
+  const [height, setHeight] = useState(180);
+  const [weight, setWeight] = useState(70);
+
+  const calculateBMI = () => {
+    const heightInMeters = height / 100;
+    setBmi(weight / (heightInMeters * heightInMeters));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    calculateBMI();
+  };
+
+  const handleWeightChange = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const handleHeightChange = (e) => {
+    setHeight(e.target.value);
+  };
+
+  const handleSliderChange = (e) => {
+    setHeight(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="bmiHeader">
+        <h1>BMI Calculator</h1>
+      </div>
+
+      <div className="bmiForm">
+        <p>Enter your height and weight to calculate your BMI</p>
+        <form onSubmit={handleSubmit}>
+          <p>wieght is:{weight}</p>
+          <input
+            type="range"
+            step={1}
+            min={40}
+            max={200}
+            onChange={handleWeightChange}
+            className="slider"
+          />
+          <p>height is:{height}</p>
+          <input
+            type="range"
+            step={1}
+            min={100}
+            max={250}
+            onChange={handleHeightChange}
+            className="slider"
+          />
+          <button className="calculateButton">Calculate</button>
+        </form>
+        <p>Your BMI is: {bmi.toFixed(1)}</p>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
